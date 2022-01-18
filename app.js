95d5fdc5a3476fc92773a45d303c962bec2a4f6b
rhmedias <rhmedias@users.noreply.github.com>
2022-01-18 14:07:26 +0100

    dynamic port for hosting has been added

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const ejs = require("ejs");
 const request = require("request");
 
 const app = express();
-const PORT = 3000;
+// const PORT = 3000;  <<-------- USE port 3000 for local testing
+const PORT = process.env.PORT; //  <<-------- USE this port for hosting on heroku
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,33 +15,31 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.listen(PORT, (req, res) => {
-    console.log(`Server is UP⬆️ on port: ${PORT}`);
+  console.log(`Server is UP⬆️ on port: ${PORT}`);
 });
 
-const quote = "My quote";
-const author = "Author";
-let datas = "";
+// let datas = "";
 
 app.get("/", (req, res) => {
-    const options = {
-        method: "GET",
-        url: "https://inspiring-quotes.p.rapidapi.com/random",
-        // qs: { author: "Albert" },
-        headers: {
-            "x-rapidapi-host": "inspiring-quotes.p.rapidapi.com",
-            "x-rapidapi-key": process.env.API_KEY,
-            useQueryString: true,
-        },
-    };
+  const options = {
+    method: "GET",
+    url: "https://inspiring-quotes.p.rapidapi.com/random",
+    // qs: { author: "Albert" },
+    headers: {
+      "x-rapidapi-host": "inspiring-quotes.p.rapidapi.com",
+      "x-rapidapi-key": process.env.API_KEY,
+      useQueryString: true,
+    },
+  };
 
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        datas = JSON.parse(body);
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    let datas = JSON.parse(body);
 
-        console.log(datas);
-        res.render("index", {
-            quoteContent: datas.quote,
-            authorName: datas.author,
-        });
+    console.log(datas);
+    res.render("index", {
+      quoteContent: datas.quote,
+      authorName: datas.author,
     });
+  });
 });
